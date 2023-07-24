93dc03511961a9e848a6445ef414ee245a839211
amit-arzooo <93219660+amit-arzooo@users.noreply.github.com>
2023-07-24 18:27:10 +0530

    Remove temporary trace logs

--- server/aws/fetchSecrets.sh
+++ server/aws/fetchSecrets.sh
@@ -2,12 +2,6 @@
 
 secrets=$(aws secretsmanager get-secret-value --secret-id "$1" --query SecretString --output text)
 
-echo "Fetched Secrets"
-echo $secrets
-
 export DB_PASSWORD=$(echo $secrets | jq .DB_PASSWORD -r)
 export JWT_SECRET=$(echo $secrets | jq .JWT_SECRET -r)
 export MAIL_PASSWORD=$(echo $secrets | jq .MAIL_PASSWORD -r)
-
-echo "Mapped Secrets"
-echo $MAIL_PASSWORD
